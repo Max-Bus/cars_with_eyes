d1c18163e2087e6e528b77e5491bf96a0a095bd2
vigne <vignet009@gmail.com>
2020-05-28 15:24:02 -0400

    added racetrack class

---
+++ RaceTrack.py
@@ -0,0 +1,14 @@
+class RaceTrack:
+
+    def __init__(self):
+        print("hello")
+        # self.grid = todo
+        # self.start = () tuple todo
+        # self.end = () tuple todo
+
+    # if given user input
+    def __init__(self, grid, start, end):
+        self.grid = grid
+        self.start = start
+        self.end = end
+
