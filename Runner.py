9258b0920555299d75d3bddd950eff7780766f29
vigne <vignet009@gmail.com>
2020-05-29 17:07:28 -0400

    runner stuff

--- Runner.py
+++ Runner.py
@@ -1,8 +1,17 @@
-from p5.p5 import *
+from p5 import *
 
+SIMH = 900
+SIMW = 1000
+
+# first thing to run
 def setup():
-    size(640, 360)
-    no_stroke()
-    background(204)
+    size(SIMW, SIMH)
+    background(200)
+
+# runs immediately after setup
+def draw():
+    print("hello")
+
 
 run()
+
