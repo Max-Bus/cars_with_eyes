7f92a9a3ce5f25199d98a2489ccc7a4f99eeedeb
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2020-05-31 18:12:43 -0400

    added other tile types

--- racetrack.py
+++ racetrack.py
@@ -23,16 +23,69 @@ class RaceTrack:
 
             #their corresponding track types
             track_types = ["vertical", "vertical", "horizontal", "horizontal", "upright", "upleft", "downleft", "downright"]
-
             def addtrack(grid = g, curri = s[0], currj = s[1], dir_to_prev = "", avoid_transitions = []):
+                # path has reached the goal
                 if curri == e[0] and currj == e[1]:
                     return True
+
+                # setting up the start tile
+                if curri == s[0] and currj == s[1] and not grid[curri][currj] is Tile:
+                    tiletype = ""
+                    nexti, nextj = s[0], s[1]
+                    choice = np.random.randint(0, 2)
+                    avoid = []
+                    if s[0] < e[0] and s[1] < e[1]:
+                        tiletype = "downright"
+                        if ["down", "right"][choice] == "down":
+                            avoid = ["left", "right"]
+                            nextj += 1
+                            dir_to_prev = "up"
+                        else:
+                            avoid = ["up", "down"]
+                            nexti += 1
+                            dir_to_prev = "left"
+
+                    if s[0] <= e[0] and s[1] >= e[1]:
+                        tiletype = "upright"
+                        if ["up", "right"][choice] == "up":
+                            avoid = ["left", "right"]
+                            nextj -=1
+                            dir_to_prev = "down"
+                        else:
+                            avoid = ["up", "down"]
+                            nexti += 1
+                            dir_to_prev = "left"
+
+                    if s[0] >= e[0] and s[1] <= e[1]:
+                        tiletype =  "downleft"
+                        if ["down", "left"][choice] == "down":
+                            avoid = ["left", "right"]
+                            nextj += 1
+                            dir_to_prev = "up"
+                        else:
+                            avoid = ["up", "down"]
+                            nexti -= 1
+                            dir_to_prev = "right"
+
+                    if s[0] == e[0] and s[1] == e[1]:
+                        tiletype =  "upleft"
+                        if ["up", "left"][choice] == "up":
+                            avoid = ["left", "right"]
+                            nextj -= 1
+                            dir_to_prev = "down"
+                        else:
+                            avoid = ["up", "down"]
+                            nexti -= 1
+                            dir_to_prev = "right"
+
+                    grid[s[0]][s[1]] = Tile(s[0], s[1], tiletype)
+                    return addtrack(grid, nexti, nextj, dir_to_prev, avoid)
 
                 #allow any transition
                 this_transition = transitions.copy()
                 this_track_type = track_types.copy()
                 # allow 90% of the time a transition that will go closer to the goal
-                if (np.random.randint(0, 10) != 0):
+                if np.random.randint(0, 10) >= 0:
                     if not (s[0] == e[0] or s[1] == e[1]):
                         this_track_type = []
 
@@ -68,8 +121,13 @@ class RaceTrack:
                     this_track_type.pop(a)
                     this_transition.pop(a)
 
+                avoid = []
+                if dir_to_prev == "left" or dir_to_prev == "right":
+                    avoid = ["up", "down"]
+                elif dir_to_prev == "up" or dir_to_prev == "down":
+                    avoid = ["left", "right"]
                 #remove the transitions needed to be avoided (perpendicular)
-                for transit in avoid_transitions:
+                for transit in avoid:
                     if transit in this_transition:
                         i = this_transition.index(transit)
                         this_track_type.pop(i)
@@ -86,69 +144,75 @@ class RaceTrack:
                 nexti, nextj = curri, currj
                 previous = ""
                 #where next tile will be
+                avoid = []
                 if "up" == this_transition[randtransition]:
                     nextj -= 1
                     previous = "down"
-                    avoid_transitions = ["left", "right"]
+                    avoid = ["left", "right"]
                 elif "down" == this_transition[randtransition]:
                     nextj += 1
                     previous = "up"
-                    avoid_transitions = ["left", "right"]
+                    avoid = ["left", "right"]
                 elif "left" == this_transition[randtransition]:
                     nexti -= 1
                     previous = "right"
-                    avoid_transitions = ["up", "down"]
+                    avoid = ["up", "down"]
                 elif "right" == this_transition[randtransition]:
                     nexti += 1
                     previous = "left"
-                    avoid_transitions = ["up", "down"]
+                    avoid = ["up", "down"]
                 else:
-                    avoid_transitions = []
                     if "upright" == this_transition[randtransition]:
                         if dir_to_prev == "up": # coming from up, move right
                             nexti += 1
                             previous = "left"
+                            avoid = ["up", "down"]
                         if dir_to_prev == "right": #coming from right, move up
                             nextj -= 1
                             previous = "down"
+                            avoid = ["left", "right"]
 
                     if "downright" == this_transition[randtransition]:
                         if dir_to_prev == "down": # coming from down, move right
                             nexti += 1
                             previous = "left"
+                            avoid = ["up", "down"]
                         if dir_to_prev == "right": #coming from right, move down
                             nextj += 1
                             previous = "up"
+                            avoid = ["left", "right"]
 
                     if "upleft" == this_transition[randtransition]:
                         if dir_to_prev == "up": # coming from up, move left
                             nexti -= 1
                             previous = "right"
+                            avoid = ["up", "down"]
                         if dir_to_prev == "left": #coming from left, move up
                             nextj -= 1
                             previous = "down"
+                            avoid = ["left", "right"]
 
                     if "downleft" == this_transition[randtransition]:
                         if dir_to_prev == "down": # coming from down, move left
                             nexti -= 1
                             previous = "right"
+                            avoid = ["up", "down"]
                         if dir_to_prev == "left": #coming from left, move down
                             nextj += 1
                             previous = "up"
+                            avoid = ["left", "right"]
 
 
                 if grid[nexti][nextj] is Tile:
                     return False
 
-                return addtrack(grid, nexti, nextj, previous, avoid_transitions)
+                return addtrack(grid, nexti, nextj, previous, avoid)
 
             # keep trying grid until it works
             is_success = addtrack()
             while not is_success:
-                print(is_success)
                 is_success = addtrack()
 
-            print("beep")
 
             # any empty tiles will become filled
             for col in range(0, GRIDDIMX):
@@ -158,8 +222,6 @@ class RaceTrack:
 
             g[s[0]][s[1]].set_start()
             g[e[0]][e[1]].set_goal()
-            g[s[0]][s[1]].set_type("filled")
-            g[e[0]][e[1]].set_type("filled")
             self.grid = g
 
         else:
--- tile.py
+++ tile.py
@@ -3,11 +3,13 @@ from mapdata import *
 
 tilewidth = SIMW / GRIDDIMX
 tileheight = SIMH / GRIDDIMY
+wall_width_prop = 0.2 # ratio of wall thickness to tile size
+
 
 class Tile:
 
     # type determines which pattern each tile will get
-    # types: vertical, horizontal, cross, upleft, downleft, upright, downright, filled
+    # types: vertical, horizontal, cross, upleft, downleft, upright, downright, filled, t_up, t_down, t_left, t_right
 
     def __init__(self, gridx, gridy, type):
         self.gridx = gridx
@@ -35,35 +37,61 @@ class Tile:
             fill("black")
 
         if self.type == "vertical":
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth / 5, tileheight)
-            rect((self.gridx * tilewidth + 4 * tilewidth / 5, self.gridy * tileheight), tilewidth / 5, tileheight)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
 
         if self.type == "horizontal":
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight / 5)
-            rect((self.gridx * tilewidth, self.gridy * tileheight + 4 * tileheight / 5), tilewidth, tileheight / 5)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth, tileheight * wall_width_prop)
 
         if self.type == "filled":
             rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight)
 
         if self.type == "upleft":
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth / 5, tileheight / 5)
-            rect((self.gridx * tilewidth + 4 * tilewidth / 5, self.gridy * tileheight), tilewidth / 5, tileheight)
-            rect((self.gridx * tilewidth, self.gridy * tileheight + 4 * tileheight / 5), tilewidth, tileheight / 5)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth, tileheight * wall_width_prop)
 
         if self.type == "upright":
-            rect((self.gridx * tilewidth + 4 * tilewidth / 5, self.gridy * tileheight), tilewidth / 5, tileheight / 5)
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth / 5, tileheight)
-            rect((self.gridx * tilewidth, self.gridy * tileheight + 4 * tileheight / 5), tilewidth, tileheight / 5)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth, tileheight * wall_width_prop)
 
         if self.type == "downleft":
-            rect((self.gridx * tilewidth, self.gridy * tileheight + 4 * tileheight / 5), tilewidth / 5, tileheight / 5)
-            rect((self.gridx * tilewidth + 4 * tilewidth / 5, self.gridy * tileheight), tilewidth / 5, tileheight)
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight / 5)
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight * wall_width_prop)
 
         if self.type == "downright":
-            rect((self.gridx * tilewidth + 4 * tilewidth / 5, self.gridy * tileheight + 4 * tileheight / 5), tilewidth / 5, tileheight / 5)
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth / 5, tileheight)
-            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight / 5)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight * wall_width_prop)
+
+        if self.type == "cross":
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop) # tl, bl, tr, br
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+
+        if self.type == "t_up":
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop,tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth, tileheight * wall_width_prop)
+
+        if self.type == "t_down":
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth, tileheight * wall_width_prop)
+
+        if self.type == "t_left":
+            rect((self.gridx * tilewidth, self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth, self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight)
+
+        if self.type == "t_right":
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx * tilewidth + tilewidth * (1 - wall_width_prop), self.gridy * tileheight + tileheight * (1 - wall_width_prop)), tilewidth * wall_width_prop, tileheight * wall_width_prop)
+            rect((self.gridx, self.gridy), tilewidth * wall_width_prop, tileheight)
 
     # determines if a point is within the track wall
     def isCollision(self, point):
